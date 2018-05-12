6450aea2363774cf7031222e1b408a17fdcf4da2
newbie <747135396@qq.com>
2018-05-12 16:16:51 +0800

    h1

---
+++ service-hi/src/main/java/com/wqn/zikao/java02.java
@@ -0,0 +1,10 @@
+package com.wqn.zikao;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class java02 extends Applet{
+    public void paint(Graphics g){
+        g.drawString("hello world!",10,20);
+    }
+}
